6ff573abf762d25036691e0a46c525c1e361183d
Ovella <oks.vella@yandex.ru>
2022-11-13 15:07:33 +0300

    added task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+int m = 1;
+int n = 20;
+int sum = 0;
+SumElements();
+Console.Write(sum);
+
+void SumElements()
+{
+    if (m <= n)
+    {
+        sum = sum + m;
+        m = m+1;
+        SumElements();
+    }
+}
